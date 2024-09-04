42d841f7b0c2263ed720eed7323a5b69dbccd3ac
Kosolapov Dmitriy <101108748+DitrioN@users.noreply.github.com>
2024-09-04 10:23:39 +0300

    Update tests.

--- src/test/java/utilCollection/WriteLogger.java
+++ src/test/java/utilCollection/WriteLogger.java
@@ -14,12 +14,14 @@ public class WriteLogger {
                 + "', введенное при создании: " + CL_CYAN + except + CL_RESET
                 + ", также '" + CL_WHITE + checkField + CL_RESET + "' отображаемое в созданном пользователе: "
                 + CL_CYAN + actual + CL_RESET + ".\n\n");
+        System.out.flush();
     }
 
     public static void differenceData (String text, Date data) {
         System.out.print("Сегодняшняя дата: " + CL_CYAN + LocalDate.now() + CL_RESET + ", "
                 + CL_WHITE + text + CL_RESET + ": "
                 + CL_CYAN + new SimpleDateFormat("yyyy-MM-dd").format(data) + CL_RESET + ".\n\n");
+        System.out.flush();
     }
 
     public static void notNull (String text, boolean bool) {
@@ -28,11 +30,13 @@ public class WriteLogger {
         } else {
             System.out.print("Объект '" + CL_WHITE + text + CL_RESET + "' равен null.\n\n");
         }
+        System.out.flush();
     }
 
     public static void showLoggerInformation (String testName, Response response) {
         System.out.print("Название теста: " + CL_WHITE + testName + CL_RESET + ".\n"
                 + "Статус код: " + CL_WHITE + response.statusCode() + CL_RESET + ".\n"
                 + "Время затраченное на получение ответа: " + CL_YELLOW + response.time() + CL_RESET + " ms.\n");
+        System.out.flush();
     }
 }
